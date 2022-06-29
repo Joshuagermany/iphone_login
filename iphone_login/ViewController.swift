0ee95ce96985c1ad3f4144a7757863c3473c8801
Joshuagermany <chosc0417@gmail.com>
2022-06-29 16:01:20 +0900

    Initial Commit

---
+++ iphone_login/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  iphone_login
+//
+//  Created by cwi on 2022/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var lblHello: UILabel!
+    @IBOutlet var txtName: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "Hello, " + txtName.text! + "!"
+    }
+    
+}
